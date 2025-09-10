8184bc0767adc644140f8e2e5378fac2630a43b0
Rocmazz <noureddine.mazzene@gmail.com>
2025-09-10 19:39:42 -0400

    Changed Both Titles, Author Name, Instructions & Descrpitions. Created The Sky Background as well as grass surface.

--- topics/instructions-challenge/js/script.js
+++ topics/instructions-challenge/js/script.js
@@ -1,24 +1,38 @@
 /**
- * Title of Project
- * Author Name
+ * Instructions Landscape Challenge
+ * Noureddine Mazzene
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Making a landscape using p5
+ * 
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Creates the canvas
 */
 function setup() {
-
+    createCanvas(400, 400)
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Sets the background, draws the landscape
 */
 function draw() {
+   //The Sky
+    background("#4a8aef")
+
+    //Grassy Hill
+    drawGrassyHill()
+}
+
+function drawGrassyHill() {
+
+//Flat Grassy Surface
+push()
+noStroke()
+fill("#86ae30");
+rect(0, 250, 400, 150)
 
 }
