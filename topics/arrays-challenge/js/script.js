57979d82eceaf6b92e46430a437eeb320d52992b
Rocmazz <noureddine.mazzene@gmail.com>
2025-10-29 20:23:37 -0400

    Finished Challenge And Added screenshot with title

--- topics/arrays-challenge/js/script.js
+++ topics/arrays-challenge/js/script.js
@@ -6,7 +6,7 @@
  */
 
 let ball1 = undefined; // Will create it with createBall()
-
+let balls = []; // Balls empty array
 /**
  * Create the canvas and the ball
  */
@@ -15,6 +15,8 @@ function setup() {
   createCanvas(400, 400);
   // Create the ball
   ball1 = createBall();
+
+//   balls.push(createBall());
 }
 
 /**
@@ -43,46 +45,54 @@ function createBall() {
  */
 function draw() {
   background("#87ceeb");
-
-  moveBall();
-  bounceBall();
-  drawBall();
+    for (let ball of balls) {
+      moveBall(ball);
+      bounceBall(ball);
+      drawBall(ball);
+    }
+//   moveBall(balls[0]);
+//   bounceBall(balls[0]);
+//   drawBall(balls[0]);
 }
 
 /**
  * Moves the ball according to its velocity
  */
-function moveBall() {
-  ball1.x += ball1.velocity.x;
-  ball1.y += ball1.velocity.y;
+function moveBall(ball) {
+  ball.x += ball.velocity.x;
+  ball.y += ball.velocity.y;
 }
 
 /**
  * Bounces the ball off the walls
  */
-function bounceBall() {
+function bounceBall(ball) {
   // Check if the ball has reached the left or right
-  const bounceX = ball1.x > width || ball1.x < 0;
+  const bounceX = ball.x > width || ball.x < 0;
   // Check if the ball has reached the top or bottom
-  const bounceY = ball1.y > height || ball1.y < 0;
+  const bounceY = ball.y > height || ball.y < 0;
 
   // Handle bouncing horizontally
   if (bounceX) {
-    ball1.velocity.x *= -1;
+    ball.velocity.x *= -1;
   }
   // Handle bouncing vertically
   if (bounceY) {
-    ball1.velocity.y *= -1;
+    ball.velocity.y *= -1;
   }
 }
 
 /**
  * Draw the ball on the canvas
  */
-function drawBall() {
+function drawBall(ball) {
   push();
   noStroke();
-  fill(ball1.fill);
-  ellipse(ball1.x, ball1.y, ball1.size);
+  fill(ball.fill);
+  ellipse(ball.x, ball.y, ball.size);
   pop();
+}
+
+function mousePressed() {
+  balls.push(createBall());
 }
