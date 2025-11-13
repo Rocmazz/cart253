6dce63505ed63f8be824ad84fda5135adec8112a
Rocmazz <noureddine.mazzene@gmail.com>
2025-11-12 19:41:00 -0500

    Step 1: Set up the example code

--- topics/data-challenge/js/script.js
+++ topics/data-challenge/js/script.js
@@ -1,24 +1,51 @@
 /**
- * Title of Project
- * Author Name
- * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Terrible New Car
+ * Noureddine Mazzene
+ *
+ * A program to generate new car model names using dinosaurs.
+ *
+ * Uses:
+ * Darius Kazemi's corpora repository
+ * https://github.com/dariusk/corpora/tree/master
  */
 
 "use strict";
 
+let carData = undefined;
+let dinosaurData = undefined;
+let langData = undefined;
+let lang = "fr";
+
+// Starts with the instruction
+let carName = "Click to generate a car name.";
+
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
+ * Load the car and dinosaur data
+ */
+function preload() {}
+
+/**
+ * Create the canvas
+ */
 function setup() {
-
+  createCanvas(600, 400);
 }
-
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
+ * Display the current main text (either instructions or a car)
+ */
 function draw() {
+  background(0);
 
+  push();
+  fill("pink");
+  textAlign(CENTER, CENTER);
+  textSize(32);
+  text(carName, width / 2, height / 2);
+  pop();
 }
+
+/**
+ * Generate a new car name
+ */
+function mousePressed() {}
